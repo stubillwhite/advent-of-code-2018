4ea6b4430f699e4da82dbf2af8ef12dbf30fd337
Stu W <stubillwhite@gmail.com>
2018-12-01 12:40:40 +0000

    Add solution for day one part two

--- src/advent_of_code_2018/day_1.clj
+++ src/advent_of_code_2018/day_1.clj
@@ -8,8 +8,21 @@
 (defn- parse-input [input]
   (->> input
        (string/split-lines)
-       (map #(Integer/parseInt %))))
+       (map #(Long/parseLong %))))
 
 (defn solution-part-one [input]
   (reduce + (parse-input input)))
+
+;; Part two
+
+(defn solution-part-two [input]
+  (->> (cycle (parse-input input))
+       (reductions + 0)
+       (reduce (fn [acc x]
+                 (if (contains? acc x)
+                   (reduced x)
+                   (conj acc x)))
+               #{})))
+
+
 
--- test/advent_of_code_2018/day_1_test.clj
+++ test/advent_of_code_2018/day_1_test.clj
@@ -7,6 +7,15 @@
   (string/replace s ", " "\n"))
 
 (deftest solution-part-one-given-example-input-then-example-result
+  (is (=  3 (solution-part-one (test-input "+1, -2, +3, +1"))))
   (is (=  3 (solution-part-one (test-input "+1, +1, +1"))))
   (is (=  0 (solution-part-one (test-input "+1, +1, -2"))))
   (is (= -6 (solution-part-one (test-input "-1, -2, -3")))))
+
+(deftest solution-part-two-given-example-input-then-example-result
+  (is (=  2 (solution-part-two (test-input "+1, -2, +3, +1"))))  
+  (is (=  0 (solution-part-two (test-input "+1, -1"))))
+  (is (= 10 (solution-part-two (test-input "+3, +3, +4, -2, -4"))))
+  (is (=  5 (solution-part-two (test-input "-6, +3, +8, +5, -6"))))
+  (is (= 14 (solution-part-two (test-input "+7, +7, -2, -7, -4")))))
+
