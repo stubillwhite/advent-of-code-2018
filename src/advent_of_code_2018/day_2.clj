c94cbf3a56f4e093ee21930decfe3004c6446633
Stu W <stubillwhite@gmail.com>
2018-12-02 11:56:28 +0000

    Add solution to day two part two

--- src/advent_of_code_2018/day_2.clj
+++ src/advent_of_code_2018/day_2.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2018.day-2
   (:require [clojure.java.io :as io]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.set :as set]))
 
 (def problem-input
   (->> (string/trim (slurp (io/resource "day-2-input.txt")))))
@@ -25,4 +26,34 @@
   (->> (parse-input input)
        (map letter-count)
        (checksum)))
+
+;; Part two
+
+(defn- different-characters [a b]
+  (->> (interleave a b)
+       (partition 2)
+       (filter (fn [[a b]] (not= a b)))))
+
+(defn- matching-characters [a b]
+  (->> (interleave a b)
+       (partition 2)
+       (filter (fn [[a b]] (= a b)))))
+
+(defn- find-correct-boxes [input]
+  (first
+   (for [a input
+         b input
+         :when (and (not= a b)
+                    (= 1 (count (different-characters a b))))]
+     [a b])))
+
+(defn- find-matching-characters [[a b]]
+  (->> (matching-characters a b)
+       (map first)
+       (string/join)))
+
+(defn solution-part-two [input]
+  (->> (parse-input input)
+       (find-correct-boxes)
+       (find-matching-characters)))
 
--- test/advent_of_code_2018/day_2_test.clj
+++ test/advent_of_code_2018/day_2_test.clj
@@ -7,13 +7,25 @@
 (defn- test-input [lines]
   (string/join "\n" lines))
 
-(def- example-input ["abcdef"
-                     "bababc"
-                     "abbcde"
-                     "abcccd"
-                     "aabcdd"
-                     "abcdee"
-                     "ababab"])
+(def- example-input-one ["abcdef"
+                         "bababc"
+                         "abbcde"
+                         "abcccd"
+                         "aabcdd"
+                         "abcdee"
+                         "ababab"])
 
 (deftest solution-part-one-given-example-input-then-example-result
-  (is (=  12 (solution-part-one (test-input example-input)))))
+  (is (= 12 (solution-part-one (test-input example-input-one)))))
+
+(def- example-input-two ["abcde"
+                         "fghij"
+                         "klmno"
+                         "pqrst"
+                         "fguij"
+                         "axcye"
+                         "wvxyz"])
+
+(deftest solution-part-two-given-example-input-then-example-result
+  (is (= "fgij" (solution-part-two (test-input example-input-two)))))
+
